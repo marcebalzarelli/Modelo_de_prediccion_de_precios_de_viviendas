e94a9cb2357237e02a28d98e26401e94341468c8
María Marcela Balzarelli <114022720+marcebalzarelli@users.noreply.github.com>
2023-09-29 18:48:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def predict(data: dict):
         return {"error": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 #uvicorn main:app --host 0.0.0.0 --port 8000
 
 #import requests
